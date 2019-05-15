c5f20464bdc3b041b7336e0269c2436f379c8d45
Jane <jane@cupist.com>
2019-05-15 15:05:29 +0900

    removed inset top

--- Sources/WaterfallLayout.swift
+++ Sources/WaterfallLayout.swift
@@ -296,8 +296,10 @@ public class WaterfallLayout: UICollectionViewLayout {
         let headerHeight = self.headerHeight(for: section)
         let headerInset = self.headerInset(for: section)
 
-        position += headerInset.top
-
+        if section != 0 {
+            position += headerInset.top
+        }
+        
         if headerHeight > 0 {
             let attributes = UICollectionViewLayoutAttributes(forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, with: [section, 0])
             attributes.frame = CGRect(
