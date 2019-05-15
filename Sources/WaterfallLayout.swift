2652aae20cb232d1209a429fe6a84b325681d773
Jane <jane@cupist.com>
2019-05-15 11:24:50 +0900

    no message

--- Sources/WaterfallLayout.swift
+++ Sources/WaterfallLayout.swift
@@ -211,6 +211,7 @@ public class WaterfallLayout: UICollectionViewLayout {
             headersAttribute[indexPath.section]?.zIndex = 9999
             return headersAttribute[indexPath.section]
         } else {
+            if indexPath.isEmpty { return nil }
             return footersAttribute[indexPath.section]
         }
     }
